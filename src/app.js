bd8be6f4ac899aede91441c079f668cd0bfe38d6
Arvianto Dwi <arvianto.dwi@gmail.com>
2021-03-03 04:40:28 +0700

    🚧 Add basic carousel feature
    - Add rating template module
    
    - Add author template module
    
    - Add product card template module
    
    - Add more static assets

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 // app.js
 import "bootstrap/dist/css/bootstrap.css";
+import "swiper/swiper-bundle.css";
+// import "swiper/swiper.scss";
+// import "swiper/components/a11y/a11y.scss";
+// import "swiper/components/navigation/navigation.scss";
 import "./scss/app.scss";
--- src/views/Index/index.js
+++ src/views/Index/index.js
@@ -1,1 +1,48 @@
 // index.js
+import Swiper, { Navigation, A11y } from "swiper/core";
+
+Swiper.use([Navigation, A11y]);
+
+const carouselElement = (idName) =>
+  document.getElementById(idName).querySelector(".swiper-container");
+const carouselDefaultOptions = {
+  direction: "horizontal",
+  loop: true,
+  slidesPerGroup: 5,
+  slidesPerView: 5,
+  spaceBetween: 24,
+  navigation: false,
+  a11y: true,
+};
+
+const lessonsCarousel = (() => {
+  const swiper = carouselElement("lessons-carousel");
+  const prevEl = swiper.parentElement.querySelector(".carousel-nav-left");
+  const nextEl = swiper.parentElement.querySelector(".carousel-nav-right");
+  return new Swiper(
+    swiper,
+    Object.assign({}, carouselDefaultOptions, {
+      navigation: { prevEl, nextEl },
+      a11y: {
+        prevSlideMessage: "Previous lessons slide",
+        nextSlideMessage: "Next lessons slide",
+      },
+    })
+  );
+})();
+
+const webinarsCarousel = (() => {
+  const swiper = carouselElement("webinars-carousel");
+  const prevEl = swiper.parentElement.querySelector(".carousel-nav-left");
+  const nextEl = swiper.parentElement.querySelector(".carousel-nav-right");
+  return new Swiper(
+    swiper,
+    Object.assign({}, carouselDefaultOptions, {
+      navigation: { prevEl, nextEl },
+      a11y: {
+        prevSlideMessage: "Previous webinars slide",
+        nextSlideMessage: "Next webinars slide",
+      },
+    })
+  );
+})();
