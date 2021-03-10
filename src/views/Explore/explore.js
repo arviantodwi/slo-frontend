762ab73cdc2012ab2d7f4c4d83b8d3d134118fc3
Arvianto Dwi <arvianto.dwi@gmail.com>
2021-03-10 10:55:10 +0700

    ✨ Preloader feature
    - App now can remember the last chosen view type and restore it when users reload the Explore page

--- src/views/Explore/explore.js
+++ src/views/Explore/explore.js
@@ -4,24 +4,27 @@ import GridContent from "./grid.html";
 import ListContent from "./list.html";
 
 const VIEW_SWITCH_CLASSNAME = "view-switch";
-const CONTENT_CLASSNAME = "content-catalog";
-const VIEW_TYPE_STORE = "explore-view-type";
 const SWITCH_ACTIVE_CLASSNAME = "switch-active";
+const CONTENT_CLASSNAME = "content-catalog";
+const CONTENT_PRELOADER_CLASSNAME = "preloader";
+const CONTENT_TOPBAR_CLASSNAME = "content-topbar";
+const CONTENT_PAGINATION_CLASSNAME = "content-pagination";
+const VIEW_TYPE_STORE = "explore-view-type";
 
 const store = localStorage;
 const content = document.querySelector(`.${CONTENT_CLASSNAME}`);
 const viewSwitch = document.querySelector(`.${VIEW_SWITCH_CLASSNAME}`);
+const topbar = document.querySelector(`.${CONTENT_TOPBAR_CLASSNAME}`);
+const pagination = document.querySelector(`.${CONTENT_PAGINATION_CLASSNAME}`);
+const preloaders = document.getElementsByClassName(CONTENT_PRELOADER_CLASSNAME);
 
 let currentViewType = store.getItem(VIEW_TYPE_STORE) ?? "grid";
-if (currentViewType !== "grid" && currentViewType !== "list") {
-  currentViewType = "grid";
-}
 
-const displayNewContentView = (viewType) => {
+const toggleContentView = (nextView) => {
   try {
-    if (viewType === "grid") {
+    if (nextView === "grid") {
       content.innerHTML = DomPurify.sanitize(GridContent());
-    } else if (viewType === "list") {
+    } else if (nextView === "list") {
       content.innerHTML = DomPurify.sanitize(ListContent());
     } else {
       throw new Error("View type other than Grid and List is not acceptable.");
@@ -31,8 +34,8 @@ const displayNewContentView = (viewType) => {
     return;
   }
 
-  currentViewType = viewType;
-  store.setItem(VIEW_TYPE_STORE, currentViewType);
+  currentViewType = nextView;
+  store.setItem(VIEW_TYPE_STORE, nextView);
 };
 
 const findEventTarget = (eventPath, elementLimit, elementToFind) => {
@@ -55,6 +58,17 @@ const findEventTarget = (eventPath, elementLimit, elementToFind) => {
   return target;
 };
 
+const toggleActiveSwitch = (nextView) => {
+  const previousView = nextView == "grid" ? "list" : "grid";
+
+  viewSwitch
+    .querySelector(`a[data-toggle="${previousView}"]`)
+    .classList.remove(SWITCH_ACTIVE_CLASSNAME);
+  viewSwitch
+    .querySelector(`a[data-toggle="${nextView}"]`)
+    .classList.add(SWITCH_ACTIVE_CLASSNAME);
+};
+
 const onViewSwitchButtonClick = (ev) => {
   ev.preventDefault();
 
@@ -69,14 +83,27 @@ const onViewSwitchButtonClick = (ev) => {
     return;
   }
 
-  viewSwitch
-    .querySelector(`a[data-toggle="${currentViewType}"]`)
-    .classList.remove(SWITCH_ACTIVE_CLASSNAME);
-  viewSwitch
-    .querySelector(`a[data-toggle="${viewTypeToDisplay}"]`)
-    .classList.add(SWITCH_ACTIVE_CLASSNAME);
-
-  displayNewContentView(viewTypeToDisplay);
+  toggleActiveSwitch(viewTypeToDisplay);
+  toggleContentView(viewTypeToDisplay);
 };
 
-viewSwitch.addEventListener("click", onViewSwitchButtonClick, false);
+(() => {
+  if (currentViewType !== "grid" && currentViewType !== "list") {
+    currentViewType = "grid";
+  }
+  if (currentViewType === "list") {
+    toggleActiveSwitch(currentViewType);
+    toggleContentView(currentViewType);
+  }
+
+  viewSwitch.addEventListener("click", onViewSwitchButtonClick, false);
+
+  setTimeout(() => {
+    content.style.display = null;
+    topbar.style.display = null;
+    pagination.style.display = null;
+    for (let p of preloaders) {
+      p.remove();
+    }
+  }, 2500);
+})();
