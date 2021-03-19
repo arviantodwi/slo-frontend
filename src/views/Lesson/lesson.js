78a01511490a264043e7f7bcf745653686091eea
Arvianto Dwi <arvianto.dwi@gmail.com>
2021-03-20 00:04:12 +0700

    🚧 Lesson page template
    - Temporary basic template for Lesson page. Advanced improvement needed.

--- src/views/Lesson/lesson.js
+++ src/views/Lesson/lesson.js
@@ -1,9 +1,27 @@
 // lesson.js
-import { Accordion } from "../../library/shared";
+import { Accordion, carouselBuilder } from "../../library/shared";
+import Swiper, { Navigation, A11y } from "swiper/core";
+
+Swiper.use([Navigation, A11y]);
 
 (() => {
   const curriculum = new Accordion(".curriculum-accordion", {
     togglePosition: "left",
     allowItemHeaderClick: true,
+    initialExpandedItems: "all",
   });
+
+  const description = new Accordion(".description-accordion");
+
+  const relatedCarousel = (() => {
+    const { container, option } = carouselBuilder("related-carousel", _, {
+      slidesPerGroup: 3,
+      slidesPerView: 3,
+      a11y: {
+        prevSlideMessage: "Previous slide",
+        nextSlideMessage: "Next slide",
+      },
+    });
+    return new Swiper(container, option);
+  })();
 })();
