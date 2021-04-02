bbc8d925dab33823c126d639c7a1bab5713ed0bc
Arvianto Dwi <arvianto.dwi@gmail.com>
2021-04-03 03:32:24 +0700

    🐛 Fix lesson page
    - Taller notes field
    - Related lessons carousel now follows the carousels pattern on home page
    - Fix curriculum section not showing up when using mobile emulation on desktop

--- src/views/Lesson/lesson.js
+++ src/views/Lesson/lesson.js
@@ -4,6 +4,28 @@ import Swiper, { Navigation, A11y } from "swiper/core";
 
 Swiper.use([Navigation, A11y]);
 
+const cloneCurriculum = () => {
+  const mobileCurriculum = document.querySelector(".mobile-curriculum");
+  if (mobileCurriculum.children.length) {
+    return;
+  }
+
+  const curriculum = document
+    .querySelector(".desktop-sb-curriculum")
+    .firstElementChild.cloneNode(true);
+
+  mobileCurriculum.appendChild(curriculum);
+
+  const mobileCurriculumAccordion = new Accordion(
+    ".mobile-curriculum .curriculum-accordion",
+    {
+      togglePosition: "left",
+      allowItemHeaderClick: true,
+      initialExpandedItems: 0,
+    }
+  );
+};
+
 (() => {
   const curriculum = new Accordion(".curriculum-accordion", {
     togglePosition: "left",
@@ -17,36 +39,35 @@ Swiper.use([Navigation, A11y]);
 
   const relatedCarousel = (() => {
     const { container, option } = carouselBuilder("related-carousel", _, {
-      slidesPerGroup: 3,
-      slidesPerView: 3,
       a11y: {
-        prevSlideMessage: "Previous slide",
-        nextSlideMessage: "Next slide",
+        prevSlideMessage: "Previous related lessons slide",
+        nextSlideMessage: "Next related lessons slide",
       },
       breakpoints: {
         320: {
+          slidesPerView: 1,
           slidesPerGroup: 1,
-          slidesPerView: 1.25,
+        },
+        768: {
+          slidesPerView: 3.5,
+          slidesPerGroup: 1,
+        },
+        992: {
+          slidesPerView: 2.5,
+          slidesPerGroup: 1,
+        },
+        1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+        1400: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
         },
       },
     });
     return new Swiper(container, option);
   })();
 
-  if (window.innerWidth <= 812) {
-    const curriculum = document
-      .querySelector(".desktop-sb-curriculum")
-      .firstElementChild.cloneNode(true);
-
-    document.querySelector(".mobile-curriculum").appendChild(curriculum);
-
-    const mobileCurriculum = new Accordion(
-      ".mobile-curriculum .curriculum-accordion",
-      {
-        togglePosition: "left",
-        allowItemHeaderClick: true,
-        initialExpandedItems: 0,
-      }
-    );
-  }
+  cloneCurriculum();
 })();
